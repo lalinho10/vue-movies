3373e2aa39a0cc5b54e8833fcdac9d7106910f9d
Eduardo Jiménez Alvarez <eduardojimenezalvarez@MacBook-Air-de-Eduardo.local>
2021-06-07 19:10:08 -0500

    Agregar y eleiminar elementos de una lista

--- src/views/Listas/CrearLista.vue
+++ src/views/Listas/CrearLista.vue
@@ -54,7 +54,8 @@
             label="Busca una película..."
             placeholder="Captura el nombre la película que deseas agregar"
             prepend-icon="mdi-database-search"
-            return-object>
+            return-object
+            @change="agregarElemento()">
 
             <template v-slot:item="data">
               <v-list-item-avatar>
@@ -66,16 +67,31 @@
                 </v-list-item-title>
               </v-list-item-content>
             </template>
-
-            <template v-slot:selection="data">
+          </v-autocomplete>
+        </v-col>
+      </v-row>
+      <v-row v-if="elementosLista.length > 0" no-gutters>
+        <v-col cols="12">
+          Elementos en la lista:
+        </v-col>
+        <v-col cols="12">
+          <v-list >
+            <v-list-item v-for="elemento in elementosLista" :key="elemento.id">
+              <v-list-item-avatar>
+                <img :src="poster(elemento.poster_path)">
+              </v-list-item-avatar>
               <v-list-item-content>
                 <v-list-item-title>
-                  {{data.item.title}}
+                  {{elemento.title}}
                 </v-list-item-title>
               </v-list-item-content>
-            </template>
-
-          </v-autocomplete>
+              <v-list-item-action>
+                <v-btn icon @click="removerElemento(elemento.id)">
+                  <v-icon color="red lighten-2">mdi-delete</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
         </v-col>
       </v-row>
     </div>
@@ -100,6 +116,7 @@ export default {
       loading: false,
       modelo: null,
       resultados: [],
+      elementosLista: [],
     }
   },
   methods: {
@@ -113,7 +130,22 @@ export default {
           this.lista.id = this.$store.state.moduloUsuario.idListaCreada;
         })
       }
-    }
+    },
+    agregarElemento() {
+      axios.post(`/list/${this.lista.id}/add_item?api_key=${process.env.VUE_APP_TMDB_API_KEY}&session_id=${this.$store.state.moduloAutenticacion.sessionId}`, {
+        media_id: this.modelo.id,
+      })
+      .then(() => {
+        this.elementosLista.push(this.modelo)
+        this.modelo = null
+      })
+    },
+    removerElemento(id) {
+      axios.post(`/list/${this.lista.id}/remove_item?api_key=${process.env.VUE_APP_TMDB_API_KEY}&session_id=${this.$store.state.moduloAutenticacion.sessionId}`, {
+        media_id: id,
+      })
+      .then(() => this.elementosLista = this.elementosLista.filter((elemento) => elemento.id !== id))
+    },
   },
   watch: {
     buscar(textoBusqueda) {
